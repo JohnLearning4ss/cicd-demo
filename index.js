3b230149b9037eef571c3bfc84810c99acc4daea
JohnLearning4s <johnpeterantony.1996@gmail.com>
2025-07-16 00:22:25 +0530

    update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Updated this file just now\n');
+  res.end('Updated this file just now1\n');
 });
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
 
-// trigger ci/cd
+
